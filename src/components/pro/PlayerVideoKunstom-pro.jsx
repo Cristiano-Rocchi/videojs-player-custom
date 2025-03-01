5ff1183fd3046149c0627fb1987513730367a988
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-03-01 16:38:33 +0100

    update style

--- src/components/pro/PlayerVideoKunstom-pro.jsx
+++ src/components/pro/PlayerVideoKunstom-pro.jsx
@@ -46,11 +46,27 @@ const PlayerVideoKunstomPro = ({
             className: "vjs-grouped-controls-start",
           });
 
+          return el;
+        }
+      }
+
+      videojs.registerComponent("GroupedControlsStart", GroupedControlsStart);
+
+      class GroupedControlsCenter extends Component {
+        constructor(player, options) {
+          super(player, options);
+          this.addClass("vjs-grouped-controls-center");
+        }
+
+        createEl() {
+          const el = videojs.dom.createEl("div", {
+            className: "vjs-grouped-controls-center",
+          });
           // Pulsante Next
           const nextButton = videojs.dom.createEl("button", {
             className: "vjs-next-button",
             innerHTML: `
-              <svg fill="#ffffff" width="20px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 51.53 51.53" xml:space="preserve" stroke="#ffffff" stroke-width="0.0005153099999999999"><g id="SVGRepo_iconCarrier"> <g> <path d="M44.9,1.963c-3.662,0-6.631,2.969-6.631,6.631V23.81c-0.285-0.324-0.617-0.609-1-0.831L6,4.926 c-1.238-0.715-2.762-0.715-4,0C0.763,5.64,0,6.961,0,8.39v36.104c0,1.43,0.763,2.75,2,3.465c0.619,0.356,1.311,0.535,2,0.535 c0.691,0,1.381-0.179,2-0.535l31.269-18.053c0.383-0.223,0.715-0.508,1-0.832v13.863c0,3.662,2.969,6.631,6.631,6.631 s6.631-2.969,6.631-6.631V8.594C51.531,4.932,48.562,1.963,44.9,1.963z"></path> </g> </g></svg>
+              <svg fill="#ffffff" width="32px" viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M208,40V216a8,8,0,0,1-16,0V142.26416l-119.65625,73.124A16.00029,16.00029,0,0,1,48,201.73535V54.26465A16.0002,16.0002,0,0,1,72.34277,40.61133L192,113.73535V40a8,8,0,0,1,16,0Z"></path> </g></svg>
             `,
           });
 
@@ -60,9 +76,10 @@ const PlayerVideoKunstomPro = ({
             }
           });
 
+          // Pulsante Prev
           const prevButton = videojs.dom.createEl("button", {
             className: "vjs-prev-button",
-            innerHTML: `<svg width="20px"  viewBox="0 0 56 56" width="56" xmlns="http://www.w3.org/2000/svg"><path d="m9.25 47.0898h5.3203c2.0625 0 3.1172-1.0781 3.1172-3.1406v-15.0703c-.0703-.2813-.1172-.5859-.1172-.8906 0-.3281.0469-.6328.1172-.9141v-15.0469c0-2.1328-1.0547-3.1171-3.1172-3.1171h-5.3203c-2.0625 0-3.1406 1.0781-3.1406 3.1171v31.9219c0 2.0625 1.0781 3.1406 3.1406 3.1406zm37.2657-.7031c1.8514 0 3.3749-1.3828 3.3749-4.0078v-28.7813c0-2.6249-1.5235-4.0312-3.3749-4.0312-.9376 0-1.7344.3047-2.6954.8672l-23.8828 14.0625c-1.2656.75-1.9687 1.6172-2.25 2.5781v1.8047c.2813.9844.9844 1.8282 2.25 2.6016l23.8828 14.039c.961.5625 1.7578.8672 2.6954.8672z"/></svg>`,
+            innerHTML: `<svg fill="#ffffff" width="32px" viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M56,32a8.00008,8.00008,0,0,1,8,8v73.73535l119.65723-73.124A16.0002,16.0002,0,0,1,208,54.26465v147.4707a16.004,16.004,0,0,1-24.34375,13.65283L64,142.26416V216a8,8,0,0,1-16,0V40A8.00008,8.00008,0,0,1,56,32Z"></path> </g></svg>`,
           });
 
           prevButton.addEventListener("click", () => {
@@ -87,7 +104,7 @@ const PlayerVideoKunstomPro = ({
 
           //skip avanti 10 secondi
           const skip10ForwardButton = videojs.dom.createEl("button", {
-            className: "vjs-skip-forward-button",
+            className: "vjs-skip-forward-button sec10",
             innerHTML: `
            <svg fill="white" width="24px"  viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><g ><path d="m21.4873.996654c-.0019-.414209-.3391-.748494-.7533-.746646-.4143.001848-.7485.339129-.7467.753342l.0089 1.98571c.0036.80942.0049 1.35311-.04 1.76445l-.0017.01526c-.4565-.50181-.9598-.96023-1.503-1.36839-1.7969-1.35006-4.0318-2.15038-6.4515-2.15038-5.93706 0-10.75 4.81294-10.75 10.75 0 5.9371 4.81294 10.75 10.75 10.75 5.9371 0 10.75-4.8129 10.75-10.75 0-.4142-.3358-.75-.75-.75s-.75.3358-.75.75c0 5.1086-4.1414 9.25-9.25 9.25-5.10863 0-9.25-4.1414-9.25-9.25 0-5.10863 4.14137-9.25 9.25-9.25 2.0836 0 4.0044.68804 5.5505 1.84962.4989.37489.9588.79909 1.3723 1.26532-.0283.00379-.0579.00738-.0888.01076-.4114.04492-.9551.04363-1.7645.04002l-1.9857-.00886c-.4142-.00185-.7515.33244-.7533.74665-.0019.41421.3324.75149.7466.75334l2.029.00905c.7549.00339 1.3845.00621 1.8907-.04906.5309-.05798 1.0203-.18758 1.4453-.51655.0678-.05245.133-.10802.1955-.1665.106-.0993.2041-.20699.2931-.3221.329-.42508.4586-.91441.5166-1.44534.0553-.50618.0524-1.13575.0491-1.89062z"/><path d="m9.75 10.4142c0-1.19004-1.43883-1.78603-2.28033-.94453l-1 1.00003c-.29289.2929-.29289.7677 0 1.0606s.76777.2929 1.06066 0l.71967-.7196v5.1893c0 .4142.33579.75.75.75s.75-.3358.75-.75z"/><path clip-rule="evenodd" d="m15 9.25c-1.5188 0-2.75 1.2312-2.75 2.75v2c0 1.5188 1.2312 2.75 2.75 2.75s2.75-1.2312 2.75-2.75v-2c0-1.5188-1.2312-2.75-2.75-2.75zm-1.25 2.75c0-.6904.5596-1.25 1.25-1.25s1.25.5596 1.25 1.25v2c0 .6904-.5596 1.25-1.25 1.25s-1.25-.5596-1.25-1.25z" fill-rule="evenodd"/></g></svg>
             `,
@@ -116,7 +133,7 @@ const PlayerVideoKunstomPro = ({
 
           //skip indietro 10 secondi
           const skip10RewindButton = videojs.dom.createEl("button", {
-            className: "vjs-skip-rewind-button",
+            className: "vjs-skip-rewind-button sec10",
             innerHTML: `
              <svg fill="white" width="24px" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m6.46967 10.4697c-.29289.2929-.29289.7677 0 1.0606s.76777.2929 1.06066 0zm1.78033 5.5303c0 .4142.33579.75.75.75s.75-.3358.75-.75zm-5.5-4c0-.4142-.33579-.75-.75-.75s-.75.3358-.75.75zm3.24903-8 .4505.59962zm-1.99903 1.99903.54801.51204.02745-.02939.02416-.03215zm.01269-4.99568c.00185-.414213-.33244-.751495-.74665-.753343s-.75149.332437-.75334.746647zm-.75885 1.98236-.75-.00334zm3.68 3.68.00334.75zm1.98906.74114c.41421-.00185.74849-.33913.74664-.75334s-.33913-.7485-.75334-.74665zm-5.26052-1.50418-.59312.45902zm.3545.3545-.45902.59312zm12.23312 5.74283v2h1.5v-2zm-2.5 2v-2h-1.5v2zm1.25 1.25c-.6904 0-1.25-.5596-1.25-1.25h-1.5c0 1.5188 1.2312 2.75 2.75 2.75zm1.25-1.25c0 .6904-.5596 1.25-1.25 1.25v1.5c1.5188 0 2.75-1.2312 2.75-2.75zm-1.25-3.25c.6904 0 1.25.5596 1.25 1.25h1.5c0-1.5188-1.2312-2.75-2.75-2.75zm0-1.5c-1.5188 0-2.75 1.2312-2.75 2.75h1.5c0-.6904.5596-1.25 1.25-1.25zm-13.75 2.75c0 5.9371 4.81294 10.75 10.75 10.75v-1.5c-5.10863 0-9.25-4.1414-9.25-9.25zm10.75 10.75c5.9371 0 10.75-4.8129 10.75-10.75h-1.5c0 5.1086-4.1414 9.25-9.25 9.25zm10.75-10.75c0-5.93706-4.8129-10.75-10.75-10.75v1.5c5.1086 0 9.25 4.14137 9.25 9.25zm-10.75-10.75c-2.41966 0-4.65456.80032-6.45148 2.15038l.90101 1.19924c1.54606-1.16158 3.46683-1.84962 5.55047-1.84962zm-6.45148 2.15038c-.81333.61107-1.53707 1.33481-2.14814 2.14814l1.19924.90101c.5262-.70037 1.14954-1.32371 1.84991-1.84991zm-3.03582-2.403726-.00886 1.985716 1.49999.00669.00886-1.98571zm4.42448 6.419056 1.98572-.00886-.0067-1.49999-1.98571.00886zm-4.43334-4.43334c-.00345.77414-.00746 1.41813.04887 1.93398.05798.53093.18758 1.02026.51655 1.44534l1.18625-.91804c-.09017-.11652-.16839-.29389-.21166-.69014-.04492-.41134-.04363-.95503-.04002-1.76445zm4.42665 2.93335c-.80942.00361-1.3531.0049-1.76445-.04002-.39625-.04327-.57362-.12149-.69014-.21166l-.91804 1.18625c.42508.32897.91441.45857 1.44534.51655.51585.05633 1.15984.05232 1.93398.04887zm.53918 3.55395-1 1.00003 1.06066 1.0606 1-1zm.78033 2.53033v4h1.5v-4zm0-1.5858v.0858h1.5v-.0858zm0 .0858v1.5h1.5v-1.5zm-5.18074-4.13831c.08838.1142.18558.2211.29066.31976l1.02669-1.09358c-.04739-.04449-.09123-.0927-.1311-.14422zm.29066.31976c.0632.05933.12925.11569.19794.16884l.91804-1.18625c-.03099-.02399-.06078-.0494-.08929-.07616zm.09207-1.19447-.12673.13563 1.09601 1.0241.12674-.13564zm5.07834 5.04332c-.0184.0184-.0508.0375-.09038.0443-.03548.0062-.06566.0008-.08858-.0087s-.04806-.027-.0688-.0564c-.02313-.0329-.03257-.0693-.03257-.0953h1.5c0-1.19004-1.43883-1.78603-2.28033-.94453z" /></svg>
             `,
@@ -129,29 +146,15 @@ const PlayerVideoKunstomPro = ({
           });
 
           // Append Button
-          el.appendChild(nextButton);
-          el.appendChild(skip30ForwardButton);
-          el.appendChild(skip10ForwardButton);
+
           el.appendChild(skip10RewindButton);
           el.appendChild(skip30RewindButton);
+
           el.appendChild(prevButton);
+          el.appendChild(nextButton);
 
-          return el;
-        }
-      }
-
-      videojs.registerComponent("GroupedControlsStart", GroupedControlsStart);
-
-      class GroupedControlsCenter extends Component {
-        constructor(player, options) {
-          super(player, options);
-          this.addClass("vjs-grouped-controls-center");
-        }
-
-        createEl() {
-          const el = videojs.dom.createEl("div", {
-            className: "vjs-grouped-controls-center",
-          });
+          el.appendChild(skip30ForwardButton);
+          el.appendChild(skip10ForwardButton);
 
           return el;
         }
@@ -192,11 +195,11 @@ const PlayerVideoKunstomPro = ({
           children: [
             {
               name: "GroupedControlsStart",
-              children: ["playToggle", "nextButton"],
+              children: ["progressControl", "muteToggle", "volumeControl"],
             },
             {
               name: "GroupedControlsCenter",
-              children: ["progressControl", "volumePanel"],
+              children: ["playToggle"],
             },
             {
               name: "GroupedControlsEnd",
